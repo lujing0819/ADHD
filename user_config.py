87e4f99da99600391b05604b498757543973d128
unknown <lu_0819@163.com>
2026-03-04 17:17:35 +0800

    second

--- user_config.py
+++ user_config.py
@@ -1,7 +1,7 @@
 from datetime import date, datetime
 from typing import Optional, List, Dict, Literal
 from pydantic import BaseModel, Field, field_validator
-
+#lujing
 # -------------------- 嵌套子模型 --------------------
 class PersonalInfo(BaseModel):
     """个人信息"""
